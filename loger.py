da1911bca6e2b1e5d4e26faa9a7f3b1384afda08
Герман <german@MacBook-Air-German.local>
2021-10-11 14:58:00 +0300

    function name added

--- loger.py
+++ loger.py
@@ -7,8 +7,10 @@ from datetime import datetime
 
 def logger(function):
     with open('new_file.txt', 'w') as f:
-        x = str(datetime.now())
-        f.write(x)
+        function_time = str(datetime.now())
+        function_name = function.__name__
+        f.write(f'{function_name} — {function_time}')
+
     def new_function():
         result=function()
         return result
@@ -17,6 +19,7 @@ def logger(function):
 @logger
 def any_function():
     print('Здесь может быть любая функция')
+    return True
 
 any_function() 
 
