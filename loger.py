a80ea261690ed807b679452ed151d04e7e757752
Герман <german@MacBook-Air-German.local>
2021-10-11 18:16:31 +0300

    route added

--- loger.py
+++ loger.py
@@ -3,25 +3,23 @@ from datetime import date
 from datetime import time 
 from datetime import datetime
 
-
-
 def logger(function):
-    def new_function(*args, **kwargs):
-        result=function(*args, **kwargs)
-        with open('new_file.txt', 'w') as f:
+    def new_function(route):
+        result=function(route)
+        with open(route, 'w') as f:
             function_time = str(datetime.now())
             function_name = function.__name__
-            f.write(f'{function_name} — {function_time}. Аргументы: {args}, {kwargs}. Возвращаемое значение: {result}')
+            f.write(f'{function_name} — {function_time}. Аргументы: {route}. Возвращаемое значение: {result}')
         return result
     return new_function
     
 @logger
-def any_function(*args, **kwargs):
+def any_function(route):
     print('Здесь может быть любая функция')
     return True
     
 
-any_function() 
+any_function('new_file.txt') 
 
 
 
