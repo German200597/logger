2fd7aaa353a916384eacf3f95496dd15729c0901
Герман <german@MacBook-Air-German.local>
2021-10-11 16:14:38 +0300

    result added

--- loger.py
+++ loger.py
@@ -11,14 +11,15 @@ def logger(function):
         with open('new_file.txt', 'w') as f:
             function_time = str(datetime.now())
             function_name = function.__name__
-            f.write(f'{function_name} — {function_time}. Аргументы: {args}, {kwargs}'. )
+            f.write(f'{function_name} — {function_time}. Аргументы: {args}, {kwargs}. Возвращаемое значение: {result}')
         return result
     return new_function
     
 @logger
-def any_function():
+def any_function(*args, **kwargs):
     print('Здесь может быть любая функция')
     return True
+    
 
 any_function() 
 
